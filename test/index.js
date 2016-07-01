e4828b71414013bd83293eaa3f99d22292c2d452
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-07-01 13:31:32 +0200

    fix(wercker): wait for MongoDB to start before starting build

--- test/index.js
+++ test/index.js
@@ -5,7 +5,6 @@ const redis = require('./support/redis');
 const users = require('./support/users');
 
 before(done => mongo.on('ready', done));
-
 beforeEach(done => redis.flushall(done));
-beforeEach(done => mongo.users.drop(done));
-beforeEach(done => mongo.users.insert(users, done));
+beforeEach(function (done) { this.timeout(10000); mongo.db.dropDatabase(done); });
+beforeEach(function (done) { this.timeout(10000); mongo.users.insert(users, done); });
--- test/support/redis.js
+++ test/support/redis.js
@@ -2,7 +2,7 @@
 
 const Redis = require('ioredis');
 
-const addr = process.env.MONGO_PORT_27017_TCP_ADDR || 'redis';
-const port = process.env.MONGO_PORT_27017_TCP_PORT || '6379';
+const addr = process.env.MONGO_PORT_6379_TCP_ADDR || 'redis';
+const port = process.env.MONGO_PORT_6379_TCP_PORT || '6379';
 
 module.exports = new Redis(port, addr);
